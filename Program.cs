784eb09839216f30713fd7a3d1f2a0d5ec47aa7e
Alsu Suyargulova <Cuyargulova@mail.ru>
2024-08-06 22:41:34 +0500

    Метод RandomSubset

--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@ string[] resultArray = RandomSubset(initialArray);
 
 Console.WriteLine("Initial Array: " + string.Join(", ", initialArray));
 Console.WriteLine("Result Array: " + string.Join(", ", resultArray));
+
+static string[] RandomSubset(string[] inputArray)
+{
+    Random random = new Random();
